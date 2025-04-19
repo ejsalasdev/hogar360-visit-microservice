50d44af0640677e216758f640eae02993b69ba4d
Edgar J. Salas <ejsalasdev@gmail.com>
2025-04-19 11:29:28 -0500

    start visit microservice project

---
+++ src/main/java/com/powerup/visitmicroservice/VisitMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.powerup.visitmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powerup/visitmicroservice/VisitMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerup.visitmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
